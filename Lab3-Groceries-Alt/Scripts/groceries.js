33347831d5948c25bf537300252d68e3456b2955
Liam Prieditis <lprieditis@gmail.com>
2021-06-05 15:19:54 -0400

    General updates for Lab 3 and Lab 4

---
+++ Lab3-Groceries-Alt/Scripts/groceries.js
@@ -0,0 +1,163 @@
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+
+	{
+		name: "Bananas - $1.00",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 1.00
+	},
+	{
+		name: "Organic Bananas - $1.25",
+		Lactose: true,
+		Nut: true,
+		Organic: true,
+		None: true,
+		price: 1.25
+	},
+	{
+		name: "Onions - $2.00",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 2.00
+	},
+	{
+		name: "Organic Onions - $2.25",
+		Lactose: true,
+		Nut: true,
+		Organic: true,
+		None: true,
+		price: 2.25
+	},	
+	{
+		name: "Bread - $2.50",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 2.50
+	},
+	{
+		name: "Spinach- $3.00",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 3.00
+	},
+	{
+		name: "Organic Spinach- $3.25",
+		Lactose: true,
+		Nut: true,
+		Organic: true,
+		None: true,
+		price: 3.25
+	},
+	{
+		name: "Eggs - $3.50",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 3.50
+	},
+	{
+		name: "Milk - $3.75",
+		Lactose: false,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 3.75
+	},
+	{
+		name: "Almond Milk - $4.00",
+		Lactose: true,
+		Nut: false,
+		Organic: false,
+		None: true,
+		price: 4.00
+	},
+	{
+		name: "Organic Oat Milk - $4.50",
+		Lactose: true,
+		Nut: true,
+		Organic: true,
+		None: true,
+		price: 4.50
+	},
+	{
+		name: "Dragon Fruit - $5.50",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 5.50
+	},
+	{
+		name: "Organic Tofu - $6.00",
+		Lactose: true,
+		Nut: true,
+		Organic: true,
+		None: true,
+		price: 6.00
+	},
+	{
+		name: "Salmon - $12.00",
+		Lactose: true,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 12.00
+	},
+	{
+		name: "Organic Raisin Walnut Cookies - $15.00",
+		Lactose: false,
+		Nut: false,
+		Organic: true,
+		None: true,
+		price: 15.00
+	},
+	{
+		name: "Chocolate Cake - $20.00",
+		Lactose: false,
+		Nut: true,
+		Organic: false,
+		None: true,
+		price: 20.00
+	}
+
+];
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, lac, nut, org, none) {
+	let product_names = [];
+	prods.sort(function(a,b){return a.price - b.price})
+	for (let i=0; i<prods.length; i+=1) {
+		if (!(lac && !prods[i].Lactose) && !(nut && !prods[i].Nut) && !(org && !prods[i].Organic) && !(none && !prods[i].None)){
+			product_names.push(prods[i].name);
+		}
+	}
+	return product_names;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+		// totalPrice = totalPrice + parseFloat(chosenProducts[i].substring(chosenProducts[i].indexOf("$")+1))
+	}
+	return totalPrice;
+}
---
+++ Lab3-Groceries-Alt/Scripts/main.js
@@ -0,0 +1,111 @@
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+document.getElementById("openByDefault").click();
+
+populateListProductChoices();
+
+function openInfo(evt, tabName) {
+
+	selectedItems();
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+
+	
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices() {
+
+    var s1 = document.getElementById("Lactose");
+	var s2 = document.getElementById("Nut");
+	var s3 = document.getElementById("Organic");
+	var s4 = document.getElementById("None");
+
+    var sp = document.getElementById("displayProduct");
+	
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+    sp.innerHTML = "";
+		
+	// obtain a reduced list of products based on restrictions
+    var optionArray = restrictListProducts(products, s1.checked, s2.checked, s3.checked, s4.checked);
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+		
+	for (i = 0; i < optionArray.length; i++) {
+			
+		var productName = optionArray[i];
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		sp.appendChild(checkbox);
+		
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName));
+		sp.appendChild(label);
+		
+		// create a breakline node and add in HTML DOM
+		sp.appendChild(document.createElement("br"));    
+	}
+}
+	
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	// build list of selected item
+	var para = document.createElement("P");
+	para.innerHTML = "You Selected: ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+	// var subtot = document.createTextNode("Sub-Total: $" + getTotalPrice(chosenProducts));
+	// var tax = document.createTextNode("Tax (13%): $" + getTotalPrice(chosenProducts)*0.13);
+	// var tot = document.createTextNode("Total: $" + Math.Round((subtot + tax)*100)/100);
+	// var br = document.createElement("br");
+		
+	// add paragraph and total price
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Sub-Total: $" + Math.round(getTotalPrice(chosenProducts)*100) / 100));
+	c.appendChild(document.createElement("br"));
+	c.appendChild(document.createTextNode("HST (13%): $" + Math.round((getTotalPrice(chosenProducts)*0.13)*100) / 100));
+	c.appendChild(document.createElement("br"));
+	c.appendChild(document.createElement("br"));
+	c.appendChild(document.createTextNode("TOTAL: $" + Math.round((getTotalPrice(chosenProducts) + getTotalPrice(chosenProducts)*0.13)*100) / 100));
+
+}
